2eeeec7363808649fda3b01308f9585927ecb1dc
Cheok Nam Kuong <egbert200120@gmail.com>
2026-05-30 18:24:17 +1000

    Yummy

--- hello_world.R
+++ hello_world.R
@@ -1,13 +1,13 @@
 
 
-```{r setup, include=FALSE}
+#+ r setup, include=FALSE
 knitr::opts_chunk$set(echo = TRUE)
-```
 
-```{r}
+
+#+ r
 library(tidyverse)
 dataset <- tibble(x = 1:5, y = c("a","b","c","d","e"), z = c("c","b","a","d","e"))
 ggplot(dataset, aes(x = x)) +
   geom_bar()
-```
+
 
